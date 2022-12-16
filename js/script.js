5c725a9c31fc170772351a672411825d245c4af8
Salvatore Capano <capano_salvatore@libero.it>
2022-12-16 14:47:02 +0100

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Inserimento Nome
+const myName = prompt("Inserisci il tuo nome: ");
+
+// Inserimento Cognome
+const myLastName = prompt("Inserisci il tuo cognome: ");
+
+// Inserimento Colore
+const myColor = prompt("Inserisci il tuo colore preferito: ");
+
+// Mostra la password
+document.getElementById("myPassword").innerHTML = myName + myLastName + myColor + "21";
